98f68d31cdeeaabc641c1e1aef3d87194c030791
xcoderman <PachieMac@Archies-MacBook-Pro.local>
2014-07-08 09:02:21 +0800

    there transfer

---
+++ SampleJSON/SampleJSON/HomeTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomeTableViewController.h
+//  SampleJSON
+//
+//  Created by Archie Angeles on 7/7/14.
+//  Copyright (c) 2014 PachieOrg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSString *deviceGUID;
+@property (nonatomic, strong) NSString *phoneNumber;
+
+@end
